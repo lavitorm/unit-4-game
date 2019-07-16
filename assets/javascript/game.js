07675bea1def29639f4a9c016c1da8d3c36341d6
Lavitor Matzembacker <51544066+lavitorm@users.noreply.github.com>
2019-07-16 09:48:21 -0700

    added functionality to buttons (crystals)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,8 +15,9 @@ var diamond= Math.floor(Math.random()*11)+1;
 var sapphire= Math.floor(Math.random()*11)+1;
 var esmerald= Math.floor(Math.random()*11)+1;
 //The app should show the number of games the player wins and loses
-var wins= 0;
-var losses = 0;
+var randomNumber = "Random Number: " + randomNumber;
+var wins = "wins: " + 0;
+var losses = "losses: " + 0;
 var score= 0; 
 $('#wins').text(wins);
 $('#losses').text(losses);
@@ -25,34 +26,35 @@ $('#losses').text(losses);
 function reset(){
     randomNumber=Math.floor(Math.random()*101+19);
     console.log(randomNumber)
-    $('#randomNumber').text(randomNumber);
+    $('#randomNumber').text("Number: " + randomNumber);
     ruby= Math.floor(Math.random()*11+1);
     diamond= Math.floor(Math.random()*11+1);
-    saphire= Math.floor(Math.random()*11+1);
+    sapphire= Math.floor(Math.random()*11+1);
     esmerald= Math.floor(Math.random()*11+1);
     score= 0;
     $('#score').text(score);
     } 
-//adds the wins to the score
+//testing the code with "alert" function (if it adds the wins/losses to the score)
 function winner(){
     alert("You win!!");
       wins++; 
       $('#wins').text(wins);
       reset();
     }
-//addes the losses to the score
 function loser(){
     alert ("You lose!");
       losses++;
       $('#losses').text(losses);
       reset();
     }
-//sets up click for crystals
+//setup click for crystals
+//win/lose conditions
+//The player wins if their total score matches the random number from the beginning of the game
+//The player loses if their score goes above the random number
   $('#ruby').on ('click', function(){
     score = score + ruby;
-    console.log("updated score= " + score);
-    $('#score').text(scoreLabel); 
-          //sets win/lose conditions
+    console.log("score= " + score);
+    $('#score').text(score); 
         if (score == randomNumber){
           winner();
         }
@@ -62,8 +64,8 @@ function loser(){
   })  
   $('#diamond').on ('click', function(){
     score = score + diamond;
-    console.log("New scoreLabel= " + score);
-    $('#scoreLabel').text(score); 
+    console.log("score= " + score);
+    $('#score').text(score); 
         if (score == randomNumber){
           winner();
         }
@@ -73,9 +75,8 @@ function loser(){
   })  
   $('#sapphire').on ('click', function(){
     score = score + sapphire;
-    console.log("Updated Score = " + score);
-    $('#scoreLabel').text(score);
-//sets win/lose conditions
+    console.log("score = " + score);
+    $('#score').text(score);
           if (score == randomNumber){
           winner();
         }
@@ -83,10 +84,10 @@ function loser(){
           loser();
         } 
   })  
-  $('#four').on ('click', function(){
+  $('#esmerald').on ('click', function(){
     score = score + esmerald;
     console.log("Updated Score = " + score);
-    $('#finalTotal').text(score); 
+    $('#scoreLabel').text(score); 
       
           if (score == randomNumber){
           winner();
